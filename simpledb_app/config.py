24082478d01eab7532e184fef383e566a7a9a11b
Cesar Garcia <csargba@gmail.com>
2024-08-28 00:54:10 -0600

    Remove run.py commit

--- simpledb_app/config.py
+++ simpledb_app/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = 'postgresql://databasetest_guv7_user:zjrvahyjGtNb4uuyqdpN577WkReUD9H8@dpg-cr6ubmi3esus7393hhlg-a.oregon-postgres.render.com/databasetest_guv7'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.office365.com'
